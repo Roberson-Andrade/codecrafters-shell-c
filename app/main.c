04feb4473e2108a635f72ad0936faa2315ac54b3
Roberson Andrade <roberson_souza11@hotmail.com>
2025-01-04 16:55:19 -0300

    REPL

--- app/main.c
+++ app/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
   // Flush after every printf
   setbuf(stdout, NULL);
 
@@ -10,10 +11,13 @@ int main() {
 
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
-  
-  input[strlen(input) - 1] = '\0';
-  
-  printf("%s: command not found\n", input);
+
+  while (fgets(input, 100, stdin) != NULL)
+  {
+    input[strlen(input) - 1] = '\0';
+
+    printf("%s: command not found\n", input);
+  }
+
   return 0;
 }
