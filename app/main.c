49248d37d267f13c0f2fddf517905ac51c116afe
Roberson Andrade <roberson_souza11@hotmail.com>
2025-03-05 22:49:12 -0300

    refactor: improve things

--- app/main.c
+++ app/main.c
@@ -9,6 +9,69 @@
 
 const char *SHELL_COMMANDS[3] = {EXIT_COMMAND, ECHO_COMMAND, TYPE_COMMAND};
 
+struct Command
+{
+  char *name;
+  char **args;
+  int argc;
+};
+
+struct Command *parse_command(char *input)
+{
+  struct Command *cmd = malloc(sizeof(struct Command));
+
+  int capacity = 5;
+  int argc = 0;
+  int is_command_name = 1;
+
+  char **args = malloc(capacity * sizeof(char *));
+
+  char *token = strtok(input, " ");
+
+  while (token != NULL)
+  {
+    if (argc == capacity)
+    {
+      capacity *= 2;
+      args = realloc(args, capacity * sizeof(char *));
+    }
+
+    char *token_ptr = malloc(strlen(token) + 1);
+    strcpy(token_ptr, token);
+
+    if (is_command_name == 1)
+    {
+      cmd->name = token_ptr;
+      token = strtok(NULL, " ");
+      is_command_name = 0;
+
+      continue;
+    }
+
+    args[argc] = token_ptr;
+
+    argc++;
+
+    token = strtok(NULL, " ");
+  }
+
+  cmd->args = args;
+  cmd->argc = argc;
+
+  return cmd;
+}
+
+void printCommand(struct Command *cmd)
+{
+  printf("name: %s\n", cmd->name);
+  printf("argc: %d\n", cmd->argc);
+
+  for (int i = 0; i < cmd->argc; i++)
+  {
+    printf("arg: %s\n", cmd->args[i]);
+  }
+}
+
 int is_shell_command(const char *input)
 {
   int result = 0;
@@ -52,6 +115,73 @@ char *search_for_command_in_path(const char *command)
   return NULL;
 }
 
+void handleExit()
+{
+  exit(0);
+}
+
+void handle_type(struct Command *cmd)
+{
+  if (cmd->argc == 0)
+  {
+    return;
+  }
+
+  char *command = cmd->args[0];
+
+  if (is_shell_command(command) == 1)
+  {
+    printf("%s is a shell builtin", command);
+  }
+  else
+  {
+    char *command_path = search_for_command_in_path(command);
+
+    if (command_path != NULL)
+    {
+      printf("%s is %s", command, command_path);
+
+      free(command_path);
+    }
+    else
+    {
+
+      printf("%s: not found", command);
+    }
+  }
+}
+
+void handle_echo(struct Command *cmd)
+{
+  if (cmd->argc == 0)
+  {
+    return;
+  }
+
+  for (int i = 0; i < cmd->argc; i++)
+  {
+    char formatter[4];
+
+    if (i == 0)
+    {
+      strcpy(formatter, "%s ");
+    }
+    else
+    {
+      strcpy(formatter, " %s");
+    }
+
+    printf(formatter, cmd->args[i]);
+  }
+
+  printf("\n");
+}
+
+void handle_not_found(struct Command *cmd)
+{
+  printf("%s: command not found", cmd->name);
+}
+
 int main()
 {
   // Flush after every printf
@@ -66,42 +196,23 @@ int main()
   {
     input[strlen(input) - 1] = '\0';
 
-    if (!strncmp(input, EXIT_COMMAND, strlen(EXIT_COMMAND)))
+    struct Command *cmd = parse_command(input);
+
+    if (!strcmp(cmd->name, EXIT_COMMAND))
     {
-      exit(0);
-      break;
+      handleExit();
     }
-
-    if (!strncmp(input, TYPE_COMMAND, strlen(TYPE_COMMAND)))
+    else if (!strcmp(cmd->name, TYPE_COMMAND))
     {
-      char *command = input + strlen(TYPE_COMMAND) + 1;
-
-      if (is_shell_command(command) == 1)
-      {
-        printf("%s is a shell builtin", command);
-      }
-      else
-      {
-        char *command_path = search_for_command_in_path(command);
-
-        if (command_path != NULL)
-        {
-          printf("%s is %s", command, command_path);
-        }
-        else
-        {
-
-          printf("%s: not found", command);
-        }
-      }
+      handle_type(cmd);
     }
-    else if (!strncmp(input, ECHO_COMMAND, strlen(ECHO_COMMAND)))
+    else if (!strcmp(cmd->name, ECHO_COMMAND))
     {
-      printf("%s", input + strlen(ECHO_COMMAND) + 1);
+      handle_echo(cmd);
     }
     else
     {
-      printf("%s: command not found", input);
+      handle_not_found(cmd);
     }
 
     printf("\n$ ");
